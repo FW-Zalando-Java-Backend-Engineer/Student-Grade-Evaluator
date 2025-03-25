fcbb5081d3d02325f322b33c2fb82742e689f756
Safwan Kher Allah <68548982+safwan-kher@users.noreply.github.com>
2025-03-25 14:14:57 +0100

    Create ScoreToGradeConverter.java

---
+++ src/ScoreToGradeConverter.java
@@ -0,0 +1,13 @@
+public class ScoreToGradeConverter {
+
+    public static char getGrade(int score) {
+        // TODO: Implement using nested if-else
+        return ' '; // placeholder
+    }
+
+    // Optional Bonus
+    public static double getGPA(int score) {
+        // TODO: Implement using nested ternary
+        return 0.0;
+    }
+}
