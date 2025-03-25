380751edf93f12f7a1f94ffcd8fdad4eccb9dbe5
Safwan Kher Allah <68548982+safwan-kher@users.noreply.github.com>
2025-03-25 14:15:42 +0100

    Create HonorsEligibilityChecker.java

---
+++ src/HonorsEligibilityChecker.java
@@ -0,0 +1,7 @@
+public class HonorsEligibilityChecker {
+
+    public static boolean isEligibleForHonors(int score, boolean hasPerfectAttendance) {
+        // TODO: Implement using logical operators
+        return false;
+    }
+}
